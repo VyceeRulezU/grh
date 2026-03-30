116554c8fb70642fdd95e23b3975c53495ef7bf2
Joseph-Brendan <199966678+Ironali@users.noreply.github.com>
2026-03-30 13:08:51 +0100

    feat: configure Vike SSR and add global head elements for assets and typography

---
+++ pages/+Head.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" type="image/png" href="/og-image.png" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <meta name="theme-color" content="#4DA771" />
+      
+      {/* DNS Preconnect for Performance */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      
+      {/* Typography & Iconography */}
+      <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    </>
+  )
+}
--- pages/+config.js
+++ pages/+config.js
@@ -1,8 +1,11 @@
 import vikeReact from 'vike-react/config'
+import Head from './+Head'
 
 export default {
   // https://vike.dev/extends
   extends: [vikeReact],
+  // Custom global head elements (Fonts, Icons, Favicon)
+  Head, 
   // https://vike.dev/ssr
   ssr: true, // Enable full SSR to get static content in HTML
   // https://vike.dev/prerender
