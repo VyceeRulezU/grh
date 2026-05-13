b294725fdf17c49feea92c90ce42dd10cd786f22
Joseph-Brendan <ironaliv@gmail.com>
2026-05-13 09:39:59 +0100

    feat: create isolated PdfViewerInner component for lazy-loaded PDF rendering

--- src/modules/research/components/PdfViewerInner.jsx
+++ src/modules/research/components/PdfViewerInner.jsx
@@ -16,7 +16,7 @@ const PdfViewerInner = ({ fileUrl }) => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@4.10.38/build/pdf.worker.min.js">
       <Viewer
         fileUrl={fileUrl}
         plugins={[defaultLayoutPluginInstance]}
